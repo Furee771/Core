b855270d6764c85a6ff787f8279ebf3b8be518b2
satto <satto@DESKTOP-RJJIM76>
2023-03-05 22:19:14 +0700

    Добавьте файлы проекта.

---
+++ Core/Program.cs
@@ -0,0 +1,30 @@
+using Core;
+using Core.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IResponseFormatter, GuidService>();
+
+var app = builder.Build();
+
+//IResponseFormatter formatter = new TextResponseFormatter();
+
+app.MapGet("/formatter1", async(HttpContext context, IResponseFormatter formatter) =>
+{
+    await formatter.Format(context, "Formatter 1");
+});
+
+app.MapGet("/formatter2", async (HttpContext context, IResponseFormatter formatter) =>
+{
+    await formatter.Format(context, "Formatter 2");
+});
+
+app.UseMiddleware<CustomMiddleware>();
+//app.UseMiddleware<CustomMiddleware2>();
+
+app.MapGet("/endpoint", CustomEndpoint.Endpoint);
+
+
+app.MapGet("/", () => "Привет Мир!");
+
+app.Run();
