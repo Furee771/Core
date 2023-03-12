6214b8499255923ddacedfd12ca05a5462585883
Furkat <57553056+Furee771@users.noreply.github.com>
2023-03-13 00:30:22 +0700

    подключил библиотеку bootstapt

--- Core/Program.cs
+++ Core/Program.cs
@@ -1,10 +1,34 @@
+using Core;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var serviceConfig = builder.Configuration;
+builder.Services.Configure<FruitOptions>(serviceConfig.GetSection("Fruit"));
 
 
 var app = builder.Build();
 
+app.Logger.LogDebug("Pipeline configuration starting");
 
 
-app.MapGet("/", () => "Привет Мир!");
+app.MapGet("/config", async (HttpContext context, IConfiguration config) =>
+{
+    string defaultDebug = config["Logging:LogLevel:Default"];
+    await context.Response.WriteAsync(defaultDebug);
+
+    string environment = config["ASPNETCORE_ENVIRONMENT"];
+    await context.Response.WriteAsync(environment);
+
+    if (app.Environment.IsDevelopment())
+    {
+        await context.Response.WriteAsync("IsDevelopment");
+    }
+});
+
+app.UseMiddleware<FruitMiddleware>();
+
+app.MapGet("/", () => "Hello Wrold!");
+
+app.UseStaticFiles();
 
 app.Run();
